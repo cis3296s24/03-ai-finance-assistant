4985fb994d6f74a54e9aa89640c6a620bbca3d94
nick-pell <93624627+nick-pell@users.noreply.github.com>
2024-04-24 19:17:00 -0400

    moved logout location on navbar and added redirection to financial advice page

--- client/src/components/financialDashboard.js
+++ client/src/components/financialDashboard.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './financialDashboard.css'; // Ensure this points to the actual CSS file location
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import "./financialDashboard.css";
 
 function FinancialDashboard() {
-  const [documentLines, setDocumentLines] = useState([createNewLine('statement')]);
-  // we will keep track of the uploads done by the user.
-  const [successfulUploads, setSuccessfulUploads] = useState(0); 
+  const navigate = useNavigate();
 
+  const [documentLines, setDocumentLines] = useState([
+    createNewLine("statement"),
+  ]);
+  // we will keep track of the uploads done by the user.
+  const [successfulUploads, setSuccessfulUploads] = useState(0);
 
   function createNewLine(documentType) {
     return {
       file: null,
       documentType: documentType,
-      startDate: '',
-      endDate: '',
-      amount: '',
-      date: ''
+      startDate: "",
+      endDate: "",
+      amount: "",
+      date: "",
     };
   }
 
@@ -32,40 +37,40 @@ function FinancialDashboard() {
   };
 
   const handleFileChange = (index, file) => {
-    if (file.type !== 'application/pdf') {
+    if (file.type !== "application/pdf") {
       alert("Please select a PDF file.");
       return;
     }
-    handleFieldChange(index, 'file', file);
+    handleFieldChange(index, "file", file);
   };
 
   const handleFileUpload = async (index) => {
     const line = documentLines[index];
-    if (line.documentType === 'statement' && !line.file) {
+    if (line.documentType === "statement" && !line.file) {
       alert("Please select a PDF file first");
       return;
     }
 
     const formData = new FormData();
     if (line.file) {
-      formData.append('file', line.file);
+      formData.append("file", line.file);
     }
-    formData.append('documentType', line.documentType);
-    if (line.startDate) formData.append('startDate', line.startDate);
-    if (line.endDate) formData.append('endDate', line.endDate);
-    if (line.amount) formData.append('amount', line.amount);
-    if (line.date) formData.append('date', line.date);
+    formData.append("documentType", line.documentType);
+    if (line.startDate) formData.append("startDate", line.startDate);
+    if (line.endDate) formData.append("endDate", line.endDate);
+    if (line.amount) formData.append("amount", line.amount);
+    if (line.date) formData.append("date", line.date);
 
     try {
-      const response = await axios.post('/upload', formData, {
+      const response = await axios.post("/upload", formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
       });
       console.log(response.data);
       alert("Data uploaded successfully");
       // icrementing success count
-      setSuccessfulUploads(prevCount => prevCount + 1)
+      setSuccessfulUploads((prevCount) => prevCount + 1);
     } catch (error) {
       console.error("Error uploading data: ", error);
       alert("Error uploading data");
@@ -74,9 +79,12 @@ function FinancialDashboard() {
 
   const handleFinalSubmit = async () => {
     try {
-      const response = await axios.post('/final-submit', { message: 'Upload complete' });
+      const response = await axios.post("/final-submit", {
+        message: "Upload complete",
+      });
       console.log(response.data);
       alert("All documents have been successfully submitted.");
+      navigate("/Financial Advice");
     } catch (error) {
       console.error("Error on final submission: ", error);
       alert("Error on final submission");
@@ -84,7 +92,7 @@ function FinancialDashboard() {
   };
 
   const addUploadLine = () => {
-    setDocumentLines([...documentLines, createNewLine('statement')]);
+    setDocumentLines([...documentLines, createNewLine("statement")]);
   };
 
   return (
@@ -104,7 +112,7 @@ function FinancialDashboard() {
               <option value="cashSpending">Cash Spending</option>
             </select>
 
-            {line.documentType === 'statement' && (
+            {line.documentType === "statement" && (
               <>
                 <input
                   type="file"
@@ -116,37 +124,49 @@ function FinancialDashboard() {
                     type="date"
                     placeholder="Start Date"
                     value={line.startDate}
-                    onChange={(e) => handleFieldChange(index, 'startDate', e.target.value)}
+                    onChange={(e) =>
+                      handleFieldChange(index, "startDate", e.target.value)
+                    }
                   />
                   <input
                     type="date"
                     placeholder="End Date"
                     value={line.endDate}
-                    onChange={(e) => handleFieldChange(index, 'endDate', e.target.value)}
+                    onChange={(e) =>
+                      handleFieldChange(index, "endDate", e.target.value)
+                    }
                   />
                 </div>
-                <button class="button" onClick={() => handleFileUpload(index)}>Upload Data</button>
+                <button class="button" onClick={() => handleFileUpload(index)}>
+                  Upload Data
+                </button>
               </>
             )}
 
-            {(line.documentType === 'income' || line.documentType === 'cashSpending') && (
+            {(line.documentType === "income" ||
+              line.documentType === "cashSpending") && (
               <>
                 <input
                   type="number"
                   placeholder="Amount"
                   value={line.amount}
-                  onChange={(e) => handleFieldChange(index, 'amount', e.target.value)}
+                  onChange={(e) =>
+                    handleFieldChange(index, "amount", e.target.value)
+                  }
                 />
                 <input
                   type="date"
                   placeholder="Date"
                   value={line.date}
-                  onChange={(e) => handleFieldChange(index, 'date', e.target.value)}
+                  onChange={(e) =>
+                    handleFieldChange(index, "date", e.target.value)
+                  }
                 />
-                <button class="button2" onClick={() => handleFileUpload(index)}>Upload Data</button>
+                <button class="button2" onClick={() => handleFileUpload(index)}>
+                  Upload Data
+                </button>
               </>
             )}
-            
           </div>
         ))}
         <button onClick={addUploadLine}>Add Another Document</button>
@@ -154,9 +174,7 @@ function FinancialDashboard() {
           <button onClick={handleFinalSubmit}>Submit All Documents</button>
         )}
       </main>
-      <footer>
-        {/* <p>&copy; 2024 MoneyTree</p> */}
-      </footer>
+      <footer>{/* <p>&copy; 2024 MoneyTree</p> */}</footer>
     </div>
   );
 }
